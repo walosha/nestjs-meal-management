ed3c0e3d25206caf5ffabb33f7980b5a652d4c0d
olawale afuye <walosha@yahoo.com>
2023-01-05 18:21:08 +0100

    additional fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.setGlobalPrefix('api');
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './users.model';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -7,5 +8,10 @@ export class UsersController {
   @Get()
   users() {
     this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    this.usersService.create(user);
   }
 }
